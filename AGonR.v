5efd5701fba0003dad0454db9fa62d444494a72c
Rainer Gonzaga <rainerdgonzaga@gmail.com>
2026-04-04 23:53:28 +0800

    feat: add odd up down counter and testbench

---
+++ AGonR.v
@@ -0,0 +1,37 @@
+// Rainer D. Gonzaga S08
+`timescale 1ns / 1ps
+
+module odd_up_down_counter (
+    Y,
+    reset,
+    clock,
+    count
+);
+
+    input Y, reset, clock;
+    output reg [3:0] count;
+
+    initial begin
+        $display("Rainer D. Gonzaga");
+        $display("Behavioral odd up-down Binary counter using D Flip Flop");
+    end
+
+    always @(posedge clock or negedge reset) begin
+        if (!reset) begin
+            count <= 4'b0001; 
+        end else begin
+            case (count)
+                4'b0001: count <= (Y) ? 4'b0011 : 4'b1111;
+                4'b0011: count <= (Y) ? 4'b0101 : 4'b0001;
+                4'b0101: count <= (Y) ? 4'b0111 : 4'b0011;
+                4'b0111: count <= (Y) ? 4'b1001 : 4'b0101;
+                4'b1001: count <= (Y) ? 4'b1011 : 4'b0111;
+                4'b1011: count <= (Y) ? 4'b1101 : 4'b1001;
+                4'b1101: count <= (Y) ? 4'b1111 : 4'b1011;
+                4'b1111: count <= (Y) ? 4'b0001 : 4'b1101;
+                default: count <= 4'b0001;
+            endcase
+        end
+    end
+
+endmodule
---
+++ AGonR_tb.v
@@ -0,0 +1,39 @@
+// Rainer D. Gonzaga S08
+`timescale 1ns / 1ps
+
+module odd_up_down_counter_tb;
+    reg Y;
+    reg reset;
+    reg clock;
+    wire [3:0] count;
+
+    odd_up_down_counter uut(Y, reset, clock, count);
+
+    initial
+    begin
+        clock = 1'b0;
+        forever #5 clock = ~clock;
+    end
+
+    initial
+    begin
+        reset = 1'b0;
+        #10 reset = 1'b1;
+    end
+
+    initial
+    begin
+        Y = 1'b0;
+        #100 Y = 1'b1;
+    end
+
+    initial
+    begin
+        $display("Rainer D. Gonzaga, Behavioral odd up-down Binary counter using D Flip Flop");
+        $monitor("time=%03d, clk=%b, reset=%b, X(Y)=%b, count=%b", $time, clock, reset, Y, count);
+        $dumpfile("AGonR.vcd");
+        $dumpvars(0, odd_up_down_counter_tb);
+    end
+
+    initial #210 $finish;
+endmodule
